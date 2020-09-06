54e35e682296129ddd6abc4e8dc0da77ed47bd30
Vladimir Titov <vtitov4@yandex.ru>
2020-09-06 15:18:18 +0400

    Initial commit

---
+++ src/main/java/ru/titov/sarafandemo/SarafanDemoApplication.java
@@ -0,0 +1,13 @@
+package ru.titov.sarafandemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarafanDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SarafanDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/titov/sarafandemo/SarafanDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.titov.sarafandemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SarafanDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
